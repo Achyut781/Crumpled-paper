5c021e8517f103b47445a93533b3c144c9768dab
Achyut781 <72300422+Achyut781@users.noreply.github.com>
2020-11-03 11:59:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	paper = new PaperBall(this.x, this.y, this.body);
+	dustbin = new Dustbin(this.x, this.y, this.body);
+	ground = new Ground(600,height,1200,20);
+
+	Engine.run(engine);
+  
+}
+
+
+
+function keyPressed(){
+	 if(keyCode === UP_ARROW){
+
+	Matter.Body.applyForce(paper.body, paper.body.position, 85, -85);	
+ }
+}
+
+function draw() {
+	rectMode(CENTER);
+	background(0);
+  
+	paper.display();
+	dustbin.display();
+	ground.display();
+	drawSprites();
+   
+  }
+
